1e3c33827973f40b196af389e716721978f524ab
Jean-André Santoni <jean.andre.santoni@gmail.com>
2011-01-31 17:11:11 +0100

    GUI: Modal windows

--- public/client.js
+++ public/client.js
@@ -1,11 +1,13 @@
 
 // Draw walkable tiles
-function display_walkables(walkables, character) {
+function display_walkables(walkables, id) {
+
+    var active = is_active(id);
 
     for (var tile in walkables) {
         var i = '#'+walkables[tile][0]+'-'+walkables[tile][1];
         $(i).addClass('walkable');
-        if ( character['active'] == 1 ) {
+        if ( active ) {
             $(i).addClass('active');
         }
     }
@@ -16,11 +18,23 @@ function display_walkables(walkables, character) {
         var y = coord[0];
         var x = coord[1];
         if ( confirm('Are you sure you want to move to '+y+','+x+'?') ) {
-            $.getJSON('http://localhost:3000/char/'+character['id']+'/moveto/'+y+'/'+x, function(data) {
-                $.getJSON('http://localhost:3000/map', map);
+            $.getJSON('http://localhost:3000/char/'+id+'/moveto/'+y+'/'+x, function(data) {
+                $.ajax({
+                    url: 'http://localhost:3000/map',
+                    type: 'get',
+                    dataType: 'json',
+                    async: false,
+                    success: map
+                });
+                display_menu(id);
             });
         }
     });
+}
+
+function is_active(id) {
+    var character = get_character(id);
+    return character['active'] == 1;
 }
 
 function get_character(id) {
@@ -78,13 +92,7 @@ function map(map) {
     // Clicking on a character
     $('.char').click(function() {
         var id = $(this).attr('id').replace('char','');
-        var character = get_character(id);
-        display_status(character);
-        if ( character['active'] == 1 ) {
-            display_active_menu(character);
-        } else {
-            display_passive_menu(character);
-        }
+        display_menu(id);
     });
     
     // Remove walkable overlay on focus out
@@ -95,48 +103,62 @@ function map(map) {
     $('#menu').empty();
 }
 
-function display_status(character) {
+function modal(content) {
+    var modal = $('<div id="modal"><div class="overlay"></div><div class="menu">'+content+'<ul></div></div>');
+    $('body').append(modal);
+    $('.overlay').click(function() {
+        modal.remove();
+    });
+    return modal;
+}
+
+function display_status(id) {
+    var character = get_character(id);
     var status = '<table>';
     status += '<tr><th>HP</th><td>'+character['hp']+'/'+character['hpmax']+'</td></tr>';
     status += '<tr><th>CT</th><td>'+character['ct']+'/'+character['ctmax']+'</td></tr>';
     status += '</table>';
-    $('#character').html(status);
+    modal(status);
 }
 
-function display_passive_menu(character) {
-    var menu = '<ul>';
-    menu += '<li id="btnMove">Show Move</li>';
-    menu += '<li id="btnStatus">Status</li>';
-    menu += '</ul>';
-    $('#menu').html(menu);
-    
-    $('#btnMove').click(function() {
-        $.getJSON('http://localhost:3000/char/'+character['id']+'/walkables', function(data) { display_walkables(data, character); } );
-    });
-}
+function display_menu(id) {
+    var character = get_character(id);
 
-function display_active_menu(character) {
     var menu = '<ul>';
-    var canmove = character['canmove'] == 1 ? 'enabled' : 'disabled';
-    menu += '<li id="btnMove" class="'+canmove+'">Move</li>';
-    menu += '<li id="btnAct">Action</li>';
+
+    if ( character['active'] == 1 ) {
+        var canmove = character['canmove'] == 1 ? 'enabled' : 'disabled';
+        menu += '<li id="btnMove" class="'+canmove+'">Move</li>';
+        var canact = character['canact'] == 1 ? 'enabled' : 'disabled';
+        menu += '<li class="'+canact+'">Action<ul class="menu"><li class="btnAttack">Attack</li></ul></li>';
+    } else {
+        menu += '<li id="btnMove">Show Move</li>';
+    }
+
     menu += '<li id="btnStatus">Status</li>';
-    menu += '<li id="btnWait">Wait</li>';
+
+    if ( character['active'] == 1 ) {
+        menu += '<li id="btnWait">Wait</li>';
+    }
     menu += '</ul>';
-    $('#menu').html(menu);
-    
+    var mdl = modal(menu);
+
     $('#btnMove').click(function() {
-        $.getJSON('http://localhost:3000/char/'+character['id']+'/walkables', function(data) { display_walkables(data, character); } );
+        $.getJSON('http://localhost:3000/char/'+id+'/walkables', function(data) { mdl.remove(); display_walkables(data, id); } );
     });
-    
-    /*$('#btnAct').click(function() {
-        $.getJSON('http://localhost:3000/char/'+character['id']+'/walkables', function(data) { display_walkables(data, character); } );
-    });*/
-    
+
+    $('#btnAttack').click(function() {
+        $.getJSON('http://localhost:3000/char/'+id+'/attackables', function(data) { display_attackables(data, id); } );
+    });
+
+    $('#btnStatus').click(function() {
+        $.getJSON('http://localhost:3000/char/'+id+'/walkables', function(data) { display_status(id); } );
+    });
+
     $('#btnWait').click(function() {
         $.getJSON(
-            'http://localhost:3000/char/'+character['id']+'/wait/'+character['direction'], 
-            function(data) { $.getJSON('http://localhost:3000/map', map);
+            'http://localhost:3000/char/'+id+'/wait/'+character['direction'], 
+            function(data) { mdl.remove(); $.getJSON('http://localhost:3000/map', map);
         } );
     });
 }
