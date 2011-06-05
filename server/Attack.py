9eb0abdc09b76616fab793c8241636e151d8db03
Jean-André Santoni <jean.andre.santoni@gmail.com>
2011-06-05 22:40:38 +0200

    Code cleaning

--- server/Attack.py
+++ server/Attack.py
@@ -3,29 +3,23 @@ import Character
 
 def GetAttackables ( party, charid ):
 
-    t1 = Character.Coords( party, charid )
-    (x1, y1, z1) = t1
+    x1, y1, z1 = Character.Coords( party, charid )
     
-    a = []
-    for tx in ( (x1-1, y1), (x1+1, y1), (x1, y1-1), (x1, y1+1) ):
+    attackables = []
+    for x2, y2 in ( (x1-1, y1), (x1+1, y1), (x1, y1-1), (x1, y1+1) ):
 
         try:
-            (x2, y2) = tx
             t2 = party['map']['tiles'][x2][y2]
             if t2 and x2 >= 0 and y2 >= 0:
                 for z2,t3 in enumerate(t2):
                     if t3 and t3['walkable'] and t3['selectable'] and math.fabs(z2-z1) <= 4:
-                        a.append( (x2, y2, z2) )
+                        attackables.append( (x2, y2, z2) )
         except:
             pass
 
-    return a
+    return attackables
 
 def IsAttackable( party, charid1, charid2 ):
-
-    attackables = GetAttackables( party, charid1 )
     
-    tile2 = Character.Coords( party, charid2 )
-    
-    return tile2 in attackables
+    return Character.Coords( party, charid2 ) in GetAttackables( party, charid1 )
 
