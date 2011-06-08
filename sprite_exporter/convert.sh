4d83af38a4827003e71ede314fa35da5946e20a8
Jean-André Santoni <jean.andre.santoni@gmail.com>
2011-06-08 14:47:54 +0200

    Fixed the file names in the sprite exporter

--- sprite_exporter/convert.sh
+++ sprite_exporter/convert.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 #for f in `ls shishi`; do ./shishi2bhps.sh "shishi/$f" "bhps/$f.png"; done
-#for f in `ls shishi`; do ./shishi2bdp.sh "shishi/$f" "bdp/$f.png"; done
-for f in `ls bdp`; do ./bdp2tethical.sh "bdp/$f" "tethical/$f.png"; done
-for f in `ls shishi`; do ./shishi2tethical_face.sh "shishi/$f" "tethical/"$f"_face.png"; done
+for f in `ls shishi`; do ./shishi2bdp.sh "shishi/$f" "bdp/"$(basename $f '.bmp')".png"; done
+for f in `ls bdp`; do ./bdp2tethical.sh "bdp/$f" "tethical/"$(basename $f '.png')".png"; done
+for f in `ls shishi`; do ./shishi2tethical_face.sh "shishi/$f" "tethical/"$(basename $f '.bmp')"_face.png"; done
