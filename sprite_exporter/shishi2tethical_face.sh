9046a302324a72db42b183392edb003c93a8b203
Jean-André Santoni <jean.andre.santoni@gmail.com>
2011-06-08 15:19:09 +0200

    Fix a sizing issue in the face sprite exporter

--- sprite_exporter/shishi2tethical_face.sh
+++ sprite_exporter/shishi2tethical_face.sh
@@ -2,4 +2,9 @@
 
 echo "converting $1 to $2"
 
-convert -extract 48x32+80+456 -rotate -90 $1 $2
+convert -size 32x64 xc:black $2
+
+convert -extract 48x32+80+456 -rotate -90 $1 /tmp/tmp.bmp
+composite -compose atop -geometry +0+8 /tmp/tmp.bmp $2 $2
+
+convert -transparent black $2 $2
